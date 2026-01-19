eaa0d833f02f8816e55412974db84d9e528143fe
JoaoGabriielDev <jgabriellima1067@gmail.com>
2026-01-18 21:19:25 -0300

    Projeto Criado

---
+++ src/main/java/com/br/coopevent/CoopeventApplication.java
@@ -0,0 +1,13 @@
+package com.br.coopevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopeventApplication.class, args);
+	}
+
+}
